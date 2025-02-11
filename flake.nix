78711c59ce03e0e7f6ae6ad946c33bd3a815f7d3
Simon Katz <simon@l3mon4.de>
2025-02-11 14:49:37 +0100

    make netns-exec behave more completely.
    imitate ip netns exec behaviour of providing non-netns aware programs
    proper /etc/resolv.conf and /etc/nsswitch.conf, and, additionally, make
    sure nscd (used by nixos) is disabled.
    
    Also overhaul the flake to provide the binary as a defaultPackage.

--- flake.nix
+++ flake.nix
@@ -1,21 +1,24 @@
 {
   description = "netns-exec - run a process in a Linux network namespace";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nix-misc = {
-    url = "github:johnae/nix-misc";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    inputs.flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "snowflake";
-      preOverlays = [
-        inputs.nix-misc.overlay
-      ];
-      systems = inputs.flake-utils.lib.defaultSystems;
-      shell = ./shell.nix;
-    }
+  outputs = { self, nixpkgs, utils, naersk}:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ rustc cargo rustPackages.clippy rustfmt bashInteractive ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    ) 
   ;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use nix::fcntl::{open, OFlag};
 use nix::sched::{setns, unshare, CloneFlags};
 use nix::{
     sys::stat::Mode,
+    mount::{
+        umount2, mount, MsFlags, MntFlags
+    },
     unistd::{setgid, setuid, Gid, Uid},
 };
 use std::env;
@@ -15,18 +18,31 @@ fn main() {
         panic!("Please supply at least 2 arguments - the network namespace name or the pid of a process whose netns you want to enter, then the command and any arguments to that command");
     };
 
-    unshare(CloneFlags::CLONE_NEWNET).expect("Failed to unshare network namespace");
+    // Mainly from https://github.com/iproute2/iproute2/blob/41710ace5e8fadff354f3dba67bf27ed3a3c5ae7/lib/namespace.c#L47
+    unshare(CloneFlags::CLONE_NEWNET | CloneFlags::CLONE_NEWNS).expect("Failed to unshare network or mount namespace. Make sure the setuid-bit is set and netns-exec owned by root.");
 
-    let nspath = if nsname.parse::<i32>().is_ok() {
-        format!("/proc/{}/ns/net", nsname)
-    } else {
-        format!("/var/run/netns/{}", nsname)
-    };
+    let nspath = format!("/var/run/netns/{}", nsname);
 
     let nsfd = open(nspath.as_str(), OFlag::O_RDONLY, Mode::empty())
         .expect(&format!("Could not open netns file: {}", nspath));
 
     setns(nsfd, CloneFlags::CLONE_NEWNET).expect("Couldn't set network namespace");
+
+    // make sure mounts don't propagate!
+    mount(None::<&str>, "/", None::<&str>, MsFlags::MS_SLAVE | MsFlags::MS_REC, None::<&str>).expect("Could not mount / as ms_slave|ms_rec");
+
+    // build env for non-namespace-aware programs.
+    umount2("/sys", MntFlags::MNT_DETACH).expect("sys was not mounted!");
+    mount(Some(nsname.as_str()), "/sys", Some("sysfs"), MsFlags::empty(), None::<&str>).expect("Could not mount netns-sys.");
+
+    let ns_etc_nsswitch = format!("/etc/netns/{}/nsswitch.conf", nsname);
+    let ns_etc_resolv = format!("/etc/netns/{}/resolv.conf", nsname);
+
+    mount(Some(ns_etc_nsswitch.as_str()), "/etc/nsswitch.conf", None::<&str>, MsFlags::MS_PRIVATE | MsFlags::MS_BIND, None::<&str>).expect(format!("Could not mount {}", ns_etc_nsswitch).as_str());
+    mount(Some(ns_etc_resolv.as_str()), "/etc/resolv.conf", None::<&str>, MsFlags::MS_PRIVATE | MsFlags::MS_BIND, None::<&str>).expect(format!("Could not mount {}", ns_etc_nsswitch).as_str());
+
+    mount(Some("/var/empty"), "/var/run/nscd", None::<&str>, MsFlags::MS_PRIVATE | MsFlags::MS_BIND, None::<&str>).expect("Could not bind-mount empty directory onto /var/run/nscd.");
+
     // drop privs now - these MUST happen in the below order, otherwise
     // dropping group privileges might fail as the user privs may have
     // changed so that the user can no longer set the gid
