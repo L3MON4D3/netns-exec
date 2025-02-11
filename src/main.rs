384e231a3e321e02aae7253989aaf4bc8ff0691c
Simon Katz <simon@l3mon4.de>
2025-02-11 15:26:27 +0100

    default to bash if no command given.

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ use std::env;
 use std::ffi::CString;
 
 fn main() {
-    let nsname = if env::args().len() > 2 {
+    let nsname = if env::args().len() > 1 {
         env::args().nth(1).unwrap()
     } else {
-        panic!("Please supply at least 2 arguments - the network namespace name or the pid of a process whose netns you want to enter, then the command and any arguments to that command");
+        panic!("Please supply at least the network-namespace to this command.");
     };
 
     // Mainly from https://github.com/iproute2/iproute2/blob/41710ace5e8fadff354f3dba67bf27ed3a3c5ae7/lib/namespace.c#L47
@@ -49,11 +49,16 @@ fn main() {
     setgid(Gid::current()).expect("Couldn't drop group privileges");
     setuid(Uid::current()).expect("Couldn't drop user privileges");
 
-    let args: Vec<_> = env::args()
-        .into_iter()
-        .skip(2)
-        .map(|arg| CString::new(arg.as_str()).unwrap())
-        .collect();
+    let args: Vec<_> = match env::args().len() {
+        // only netns-name given.
+        2 => vec![CString::new("bash").unwrap()],
+        // more commands, skip bin-name and netns-name.
+        _ => env::args()
+                .into_iter()
+                .skip(2)
+                .map(|arg| CString::new(arg.as_str()).unwrap())
+                .collect()
+    };
 
     let c_args: Vec<_> = args.iter().map(|arg| arg.as_c_str()).collect();
 
